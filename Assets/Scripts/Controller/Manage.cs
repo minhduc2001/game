6f0b471b211be9dcc655c913c3d12d3e6433453c
annh <annh@aladintech.co>
2022-12-12 16:02:55 +0700

    long

---
+++ Assets/Scripts/Controller/Manage.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Manage : MonoBehaviour
+{
+    public Canvas canvasSetting;
+    public Button btnSetting;
+    public Button btnStartGame;
+    public Button btnQuitGame;
+    public Animator animator;
+
+    private void Awake()
+    {
+        canvasSetting.gameObject.active = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnSetting.onClick.AddListener(handleBtnSettingClick);
+        btnStartGame.onClick.AddListener(handleStartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canvasSetting.gameObject.active)
+        {
+            btnSetting.interactable = true;
+        }
+    }
+
+    void handleBtnSettingClick()
+    {
+        canvasSetting.gameObject.active = true;
+        btnSetting.interactable = false;
+    }
+
+    void handleStartGame()
+    {
+        StartCoroutine(loadGame());
+    }
+
+    IEnumerator loadGame()
+    {
+        animator.SetBool("Start", true);
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(1);
+    }
+}
